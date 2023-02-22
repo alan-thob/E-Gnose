4da274cb7ae4c2e75d7e8240a8a3161dcb1259f0
Cresus83 <alan.thob@hotmail.fr>
2023-02-22 18:52:14 +0100

    update search bar

---
+++ assets/js/search-bar.js
@@ -0,0 +1,19 @@
+let select = document.getElementById("select");
+let list = document.getElementById("list");
+let selectText = document.getElementById("selectText");
+let inputfield = document.getElementById("inputfield");
+
+let options = document.getElementsByClassName("options");
+
+
+
+select.onclick = function () {
+    list.classList.toggle("open");
+}
+
+for(option of options) {
+    option.onclick= function(){
+        selectText.innerHTML = this.innerHTML;
+        inputfield.placeholder = "Rechercher dans " + selectText.innerHTML;
+    }
+}
--- index.php
+++ index.php
@@ -25,7 +25,6 @@
     <link href="assets/img/favicon.ico" rel="icon">
     <link rel="stylesheet" type="text/css" media="screen" href="assets/css/home.css">
     <link href="assets/css/navbar.css" rel="stylesheet">
-    <script src="assets/js/main.js" async></script>
 
 
 </head>
@@ -37,6 +36,22 @@ include_once('_navbar/navbar.php');
 ?>
 
 <header>
+    <div class="carousel-items">
+        <h1>Films, livres, audios … Toute une bibliothèque<br>pour vous divertir, où que vous soyez, en illimité !</h1>
+        <div class="search__bar">
+            <div id="select">
+                <p id="selectText">Toute catégorie</p>
+                <img src="assets/img/arrow-drop-down-fill.svg" alt="">
+                <ul id="list">
+                    <li class="options">Toutes les catégories</li>
+                    <li class="options">Catégorie 1</li>
+                    <li class="options">Catégorie 2</li>
+                    <li class="options">Catégorie 3</li>
+                </ul>
+            </div>
+            <input type="text" id="inputfield" placeholder="Rechercher un film, un livre, un audio, un ISBN ...">
+        </div>
+    </div>
     <div id="carouselExampleIndicators" class="carousel slide my-carousel my-carousel" data-ride="carousel">
         <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -59,7 +74,6 @@ include_once('_navbar/navbar.php');
             <span class="sr-only">Next</span>
         </a>
     </div>
-
 
 </header>
 
@@ -160,9 +174,17 @@ include_once('_navbar/navbar.php');
     </div>
 </section>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+        integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+        integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+        crossorigin="anonymous"></script>
+<script src="assets/js/carousel.js" async></script>
+<script src="assets/js/search-bar.js" async></script>
 
 </body>
 </html>
