9db31b3d135e85c7f201d9d16b1e6a23d845b32e
Cresus83 <alan.thob@hotmail.fr>
2023-02-22 02:03:55 +0100

    first update

---
+++ index.php
@@ -0,0 +1,217 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="#"/>
+    <meta name="robots" content="index, follow"/>
+    <meta property="og:title" content="Accueil | E-Gnose"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:image" content="#"/>
+    <meta property="og:url" content="https://e-gnose.sfait.fr/"/>
+    <meta property="og:description" content="#"/>
+    <meta property="og:locale" content="fr_FR"/>
+    <meta name="twitter:card" content="summary"/>
+    <meta name="twitter:title" content="Accueil | E-Gnose"/>
+    <meta name="twitter:description" content="#"/>
+    <meta name="twitter:image" content="#"/>
+    <title>Accueil | E-Gnose</title>
+
+    <!-- Links -->
+    <link href="assets/img/favicon.ico" rel="icon">
+
+    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/home.css">
+    <link href="assets/css/navbar.css" rel="stylesheet">
+    <script src="assets/js/main.js" async></script>
+
+
+</head>
+
+<body class="unselectable">
+
+<?php
+include_once('_navbar/navbar.php');
+?>
+
+
+<div class="container">
+    <div class="title">Carousel en boucle</div>
+    <div id="carousel0">
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 1 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 2 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 3 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 4 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 5 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 6 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+    </div>
+    <div class="title">Carousel infini</div>
+    <div id="carousel2">
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 1 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 2 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 3 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 4 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 5 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 6 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+    </div>
+    <div class="title">Carousel 4 éléments avec défilement 2 par 2</div>
+    <div id="carousel3">
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 1 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 2 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 3 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 4 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 5 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item__image">
+                <img src="assets/img/carousel.jpg" alt="">
+            </div>
+            <div class="item__body">
+                <div class="item__title">Mon titre 6 </div>
+                <div class="item__description">Ici une petite description pour tester le carousel </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+</body>
+</html>
