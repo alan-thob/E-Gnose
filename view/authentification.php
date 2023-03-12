219ec71f1e78ee012a65c8731a0504d017081c5c
Kyllian Diochon <kyllian.diochon.kd@gmail.com>
2023-03-12 22:25:29 +0100

    Update authentification.php
    test

--- view/authentification.php
+++ view/authentification.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!doctype html>
 <html lang="fr">
 
@@ -64,8 +65,58 @@
 
                                             </form>
                                             <?php
-                                            require_once('../controller/ControllerConnexion.php');
+                                            if (isset($_POST['connexion'])) {
+
+                                                extract($_POST);
+
+                                                if (!empty($user_email) && !empty($user_password)) {
+
+                                                    if (isset($_POST['user_email'])) {
+                                                        $req = $db->prepare('SELECT * FROM users WHERE user_email = ?');
+                                                        $req->execute(array($user_email));
+                                                        $result = $req->fetch();
+
+                                                        if ($result == true) {
+                                                            if (password_verify($user_password, $result['user_password'])) {
+                                                                session_start();
+                                                                $_SESSION["id_user"] = $result['id_user'];
+                                                                $_SESSION["user_nom"] = $result['user_nom'];
+                                                                $_SESSION["user_role"] = $result['user_role'];
+                                                                $_SESSION["user_value"] = $result['user_value'];
+                                                                if ($result['user_role'] == 1 && $result['user_value'] == 1) {
+                                                                    header('location: /index.php');
+                                                                } elseif ($result['user_role'] == 2 && $result['user_value'] == 1) {
+                                                                    header('location: /index.php');
+                                                                } elseif ($result['user_role'] == 3 && $result['user_value'] == 1) {
+                                                                    header('location: /index.php');
+                                                                } else {
+                                                                    header('location: 404.html');
+                                                                }
+                                                            } else {
+                                                                echo '
+                                                       <div class="box">
+                                                       <form class= "formBloc">
+                                                        <div class="mdpPerdu">
+                                                        Le mot de passe est incorrect, réssayez !
+                                                        </div>
+                                                       </form>
+                                                       </div>';
+                                                            }
+                                                        } else {
+                                                            echo '
+                                                       <div class="box">
+                                                       <form class= "formBloc">
+                                                        <div class="mdpPerdu">
+                                                        Les identifiants saisies ne sont pas valides
+                                                        </div>
+                                                       </form>
+                                                       </div>';
+                                                        }
+                                                    }
+                                                }
+                                            }
                                             ?>
+
                                             <p class="mb-0 mt-4 text-center"><a href="#" class="link">Mot de passe
                                                     oublié ?</a></p>
                                         </div>
