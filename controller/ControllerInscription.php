7e90a6d29f2009702b1a53eba6fc0744bb0723a1
Kyllian Diochon <kyllian.diochon.kd@gmail.com>
2023-03-12 11:14:32 +0100

    Add and modification of inscription
    Je t'ai ajouté une page "ControllerInscription" qui permet à l'utilisateur de s'inscrire dans la base de donnée (donc sur le site), pour ça j'ai modifié tous le formulaire "inscription" en rajoutant des champs et un FORMULAIRE. Il faut que tu me permettes de push entièrement le projet pour que je puisse voir les modification en temps réel pour pouvoir faire les modifications et test...

---
+++ controller/ControllerInscription.php
@@ -0,0 +1,24 @@
+<?php
+require_once('../controller/singleton_connexion.php');
+if (isset($_POST['inscription'])) {
+
+    extract($_POST);
+
+    if (!empty($user_email) && !empty($user_password) && !empty($user_password_2)) {
+
+        if ($user_password == $user_password_2) {
+            $option = ['cost' => 12,];
+            $hashpass = password_hash($user_password, PASSWORD_BCRYPT, $option);
+            if (isset($_POST['user_nom'])) {
+                $req = $db->prepare('INSERT INTO users(user_nom,user_prenom,user_email,user_password,user_telephone,user_city,user_date_naissance,user_role,user_value,id_abonnement )
+                                                                VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?,?)');
+                $req->execute(array(
+                    $user_nom,
+                    $user_prenom, $user_email, $hashpass,
+                    $user_telephone, $user_city, $user_date_naissance, 3, 1, 1
+                ));
+                echo "inscription réussite";
+            }
+        }
+    }
+}
