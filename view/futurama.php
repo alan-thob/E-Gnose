f62f5a7ca49fc9c20dd2ac6ed720da4ce9006490
Cresus83 <alan.thob@hotmail.fr>
2023-03-12 22:37:00 +0100

    update details page
    UI not finished.

--- view/futurama.php
+++ view/futurama.php
@@ -27,7 +27,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet">
     <link href="https://e-gnose.sfait.fr/assets/img/favicon.png" rel="icon">
     <link href="https://cdn.usebootstrap.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="../assets/css/content-details.css" rel="stylesheet" type="text/css" media="screen">
 </head>
 
@@ -37,29 +36,24 @@
 include_once('../_navbar/navbar.php');
 ?>
 
-<div class="content">
-        <div class="details--content__section">
-            <div class="poster--content">
-                <img src="../assets/img/posters/affiche-futurama.png" alt="" class="">
-            </div>
-
-            <div class="details--content">
-                <div class="title--content">
-                    <h1>Futurama</h1>
-                </div>
-                <div class="description--content">
-                    <p>Accidentellement cryogénisé le 31 décembre 1999 alors qu'il livrait une pizza, Fry se réveille
-                        1000
-                        plus tard à New York. A l'aube de l'an 3000, le monde a bien changé, peuplé de robots et
-                        d'extrarrestres. Le jeune homme retrouve l'un de ses descendants qui l'engage lui et ses
-                        nouveaux
-                        amis, Leela et Bender, au Planet Express, une entreprise de livraison. Ensemble, ils vont devoir
-                        faire face à de périlleuses et délirantes missions dans un monde des plus surprenants.
-                    </p>
-                </div>
+<div class="details--content__section">
+    <div class="poster--content">
+        <div class="infos--content">
+            <h1>Futurama</h1>
+            <p>Réalisateur : Matt Groening</p>
+            <div class="stars">
+                <p>Avis des internautes  </p>
+                <i class="fa fa-star gold"></i>
+                <i class="fa fa-star gold"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <p>  2/5</p>
             </div>
         </div>
+
     </div>
+</div>
 
 
 <?php
