85e225dd1c6d463fc6a37cc551e1dcfb69cdd9a9
Kyllian Diochon <kyllian.diochon.kd@gmail.com>
2023-03-14 17:57:51 +0100

    Update ControllerConnexion.php

--- controller/ControllerConnexion.php
+++ controller/ControllerConnexion.php
@@ -15,7 +15,7 @@
                         $_SESSION["user_nom"] = $result['user_nom'];
                         $_SESSION["user_role"] = $result['user_role'];
                         $_SESSION["user_value"] = $result['user_value'];
-                        header('Location : ./../index.php');
+                        header('Location : ../../../index.php');
                         exit;
                     } else {
                         echo '
