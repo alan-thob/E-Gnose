865bb0eeb7a65396c87bdf43adb41d2dcc67153b
Kyllian Diochon <kyllian.diochon.kd@gmail.com>
2023-03-15 10:24:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php session_start();
 require_once("./controller/singleton_connexion.php");
-if ($_SESSION['user_nom']) {
+if (isset($_SESSION['user_nom'])) {
     echo "Bonjour, " . $_SESSION["user_nom"];
 } else {
     echo "pensez à vous inscrire";
