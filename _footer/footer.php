fbcf2da8e53e2f770bfeb690e756a9932cf28ecf
Cresus83 <alan.thob@hotmail.fr>
2023-03-25 18:18:10 +0100

    improved navbar and footer

--- _footer/footer.php
+++ _footer/footer.php
@@ -1,7 +1,10 @@
 <div class="footer">
     <div class="footer" id="copyright">
-        <p class="text-center">&copy; #DATE# e-Gnose. Tous droits réservés.<br> <a href="#">Conditions d'utilisation et Politique de confidentialité</a> <span>|</span> <a href="#">Avertissement relatif aux cookies</a><br>
-        Fait avec ♡ par Alan THOB et Kyllian DIOCHON</p>
+        <p class="text-center">&copy; #DATE# e-Gnose. Tous droits réservés.</p>
+        <div class="button-footer">
+            <a href="#">Conditions d'utilisation et Politique de confidentialité</a> <span>|</span> <a href="#">Avertissement relatif aux cookies</a>
+        </div>
+        <p class="text-center">Fait avec ♡ par Alan THOB et Kyllian DIOCHON</p>
     </div>
 </div>
 
