ecb2d9486925209515bcaf9d00b7c0c84878584f
Kyllian Diochon <kyllian.diochon.kd@gmail.com>
2023-03-27 17:17:03 +0200

    Don't fucking touch the bdd connexion XD

--- controller/singleton_connexion.php
+++ controller/singleton_connexion.php
@@ -9,10 +9,10 @@ class Database
     //private $user = "engk9490";
     //private $pass = "p]bbxAH@7cNy";
 
-    private $host = "localhost";
-    private $dbname = "e-gnose";
-    private $user = "root";
-    private $pass = "root";
+    private $host = "telchak.o2switch.net";
+    private $dbname = "engk9490_e-gnose";
+    private $user = "engk9490";
+    private $pass = "p]bbxAH@7cNy";
 
     // Paramètre de connexion à la base
     private function __construct()
