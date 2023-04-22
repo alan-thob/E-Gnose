e430ee7be7380c01ec60d2f18458a29c773c0238
Alan THOB <alan.thob@hotmail.fr>
2023-04-22 17:03:36 +0200

    Add Trailer Play button

--- model/films_model.php
+++ model/films_model.php
@@ -37,28 +37,35 @@ class Film
                 $film_value = $film['film_value'];
             }
             if ($film_value == 1) {
-                echo "<div class='details--content__section' style='background: linear-gradient(rgb(21, 192, 237, 0.5), rgb(7, 30, 83, 1)), url($film_back_image);background-repeat: no-repeat !important;
-                background-size: cover;
-                background-position: center;'>
+                echo "<div class='details--content__section' style='background: linear-gradient(rgb(21, 192, 237, 0.5), rgb(7, 30, 83, 1)), url($film_back_image);background-repeat: no-repeat !important;background-size: cover;background-position: center;'>
+                        <div class='container-play-btn'>
+                            <a class='button is-play' href='#'> <!--Insérer le lien du média-->
+                                <div class='button-outer-circle has-scale-animation'></div>
+                                <div class='button-outer-circle has-scale-animation has-delay-short'></div>
+                                <div class='button-icon is-play'>
+                                    <svg height='100%' width='100%' fill='#fff'>
+                                    <polygon class='triangle' points='5,0 30,15 5,30' viewBox='0 0 30 15'></polygon>
+                                    <path class='path' d='M5,0 L30,15 L5,30z' fill='none' stroke='#fff' stroke-width='1'></path>
+                                    </svg>
+                                </div>        
+                            </a>
+                        </div>
                     <div class='poster--content'>
                         <div class='infos--content'>
                             <h1> $film_titre ($film_date[0])</h1>
                             <p>Réalisateur : $film_realisateur </p>
-                            <div class='play-trailer-button'>
-                                <a class='play-trailer' href='#'>Lire</a>
-                            </div>
                             <p>Duree : $film_duree (min)</p>
+                            
                             <div class='stars'>
                                 <p>Avis des internautes </p>
-                                <i class='fa fa-star'></i>
-                                <i class='fa fa-star'></i>
-                                <i class='fa fa-star'></i>
+                                <i class='fa fa-star gold'></i>
+                                <i class='fa fa-star gold'></i>
+                                <i class='fa fa-star gold'></i>
                                 <i class='fa fa-star'></i>
                                 <i class='fa fa-star'></i>
                                 <p> $film_popularity/5</p>
                             </div>
                         </div>
-            
                     </div>
                 </div>
             ";
@@ -67,3 +74,4 @@ class Film
     }
 }
 $film = new Film();
+
