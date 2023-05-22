d594b3173b9f5a0abc1ce1e10954f48ed165de09
Alan THOB <alan.thob@hotmail.fr>
2023-05-22 09:36:17 +0200

    Improved Carousel responsive and add message when empty media feedback

--- model/comment_model.php
+++ model/comment_model.php
@@ -1,5 +1,6 @@
 <?php
 require_once("../controller/singleton_connexion.php");
+
 class Commentaire
 {
     public function getComment()
@@ -7,25 +8,28 @@ class Commentaire
         global $db;
         $id = $_GET["id"];
         $req = $db->prepare('SELECT DISTINCT commentaires.*,users.user_nom FROM `users`,`laisser`,`commentaires`,`films` 
-        WHERE users.id_user = laisser.id_user AND laisser.id_commentaire = commentaires.id_commentaire AND laisser.id_film = films.id_film
-        AND commentaires.commentaire_value = 1 AND films.id_film = ?');
+ WHERE users.id_user = laisser.id_user AND laisser.id_commentaire = commentaires.id_commentaire AND laisser.id_film = films.id_film
+ AND commentaires.commentaire_value = 1 AND films.id_film = ?');
         $req->bindValue(1, $id);
         $req->execute();
         if ($req->rowCount() > 0) {
-            while ($result = $req->fetch()){
-                echo  "
-                <div class='container'>
-                <div class='user_feedback--container'>
-                <p>Auteur(e) : " . $result['user_nom'] . "</p>
-                <h6 class='text name'>" . strip_tags($result['commentaire_text']) . "</h6>
-                <p>Note attribuée : " . $result['commentaire_note'] . " étoiles." . "</p><br>
-                </div>
-                </div>";
+            while ($result = $req->fetch()) {
+                echo "
+ <div class='container'>
+ <div class='user_feedback--container'>
+ <p>Auteur(e) : " . $result['user_nom'] . "</p>
+ <h6 class='text name'>" . strip_tags($result['commentaire_text']) . "</h6>
+ <p>Note attribuée : " . $result['commentaire_note'] . " étoiles." . "</p><br>
+ </div>
+ </div>";
 
             }
+        } else {
+            echo "<p>Aucun commentaire n'a été publié.</p>";
         }
     }
 }
+
 $comment = new Commentaire();
 
 ?>
