e48eb7cd283486366a0c4bd52b4d796e56047524
Alan THOB <alan.thob@hotmail.fr>
2023-05-23 22:16:53 +0200

    Fixed navbar responsive bug

--- _navbar/navbar.php
+++ _navbar/navbar.php
@@ -9,8 +9,8 @@
 
                 <li>
                     <a href="#" class="desktop-link">Catégories</a>
-                    <input type="checkbox" id="show-features">
-                    <label for="show-features">Catégories</label>
+                    <input type="checkbox" id="category-dropdown">
+                    <label for="category-dropdown">Catégories</label>
                     <ul>
                         <li><a href="https://e-gnose.sfait.fr/view/film.php">Films</a></li>
                         <li><a href="https://e-gnose.sfait.fr/view/serie.php">Séries</a></li>
@@ -19,48 +19,73 @@
                 </li>
                 <?php
                 if (isset($_SESSION['id_user']) && $_SESSION['user_role'] == 1) {
-                    echo '<li><a href="https://e-gnose.sfait.fr/index-admin.php.php">Administration</a></li>';
+                    echo '<li><a href="https://e-gnose.sfait.fr/index-admin.php">Administration</a></li>';
                 }
                 ?>
                 <li><a href="https://e-gnose.sfait.fr/view/wishlist.php">Ma wishlist</a></li>
+
+                <div class="responsive-navbar">
+                    <?php
+                    if (isset($_SESSION['id_user']) && $_SESSION['id_user'] != null) {
+                        echo '<li>
+<input type="checkbox" id="user-area-dropdown">
+                        <label for="user-area-dropdown">Mon compte</label>
+                            <ul>
+                                <li><a href="https://e-gnose.sfait.fr/view/account_user.php">Accéder à mon compte</a></li>
+                                <li><a href="../_navbar/deconnexion.php">Se déconnecter</a></li>
+                            </ul>';
+                    } else {
+                        echo '
+                    <li><a href="https://e-gnose.sfait.fr/view/authentification.php">Mon compte</a></li></li>';
+                    }
+                    ?>
+                </div>
             </ul>
+
+
             <div class="logo">
                 <a href="https://e-gnose.sfait.fr/"><span>e</span>-Gnose</a>
             </div>
+
+            <div class="desktop-navbar">
+                <?php
+                if (isset($_SESSION['id_user']) && $_SESSION['id_user'] != null) {
+                    echo ' <div class="content-navbar" style="right: 45px;">
+                            <ul class="links">
+                                 <li>
+                                      <button class="icons2 desktop-link"><a href="#" style="padding: 22px 22px 12px 22px;"><i class="far fa-user-circle"></i></a></button>
+                                      <input type="checkbox" id="show-user-features">
+                                       <label for="show-user-features"><i class="far fa-user-circle"></i></label>
+                                           <ul style="right: 50px">
+                                              <li><a href="https://e-gnose.sfait.fr/view/account_user.php">Mon compte</a></li>
+                                             <li><a href="../_navbar/deconnexion.php">Se déconnecter</a></li>
+                                          </ul>
+                               </li>
+                             </ul>
+                             </div>';
+                } else {
+                    echo '<div class="content-navbar" style="right: 45px;padding: 21px 14px 13px 20px;">
+                                <ul class="links">
+                                   <li>
+                                       <button class="icons2 desktop-link"><a href="https://e-gnose.sfait.fr/view/authentification.php" style="padding: 22px 22px 12px 22px;"><i class="far fa-user-circle"></i></a></button>
+                                          <input type="checkbox" id="show-features">
+                                     <label for="show-features"><i class="far fa-user-circle"></i></label>
+                                  </li>
+                              </ul>
+                           </div>';
+                }
+                ?>
+            </div>
+
         </div>
 
-        <?php
-        if (isset($_SESSION['id_user']) && $_SESSION['id_user'] != null) {
-            echo '<div class="content-navbar" style="right: 45px;">
-                    <ul class="links">
-                        <li>
-                            <button class="icons2 desktop-link"><a href="#" style="padding: 22px 22px 12px 22px;"><i class="far fa-user-circle"></i></a></button>
-                            <input type="checkbox" id="show-features">
-                            <label for="show-features"><i class="far fa-user-circle"></i></label>
-                                <ul style="right: 50px">
-                                    <li><a href="https://e-gnose.sfait.fr/view/account_user.php">Mon compte</a></li>
-                                    <li><a href="../_navbar/deconnexion.php">Se déconnecter</a></li>
-                                </ul>
-                        </li>
-                    </ul>
-                    </div>';
-        } else {
-            echo '<div class="content-navbar" style="right: 45px;padding: 21px 14px 13px 20px;">
-                     <ul class="links">
-                        <li>
-                            <button class="icons2 desktop-link"><a href="https://e-gnose.sfait.fr/view/authentification.php" style="padding: 22px 22px 12px 22px;"><i class="far fa-user-circle"></i></a></button>
-                                <input type="checkbox" id="show-features">
-                                <label for="show-features"><i class="far fa-user-circle"></i></label>
-                        </li>
-                     </ul>
-                  </div>';
-        }
-        ?>
 
         <label for="show-search" class="icons search-icon"><i class="fas fa-search"></i></label>
         <form method="get" action="" class="search-box">
-            <input type="search" name="searchdata" id="searchdata" onkeyup="showMovie(this.value)" placeholder="Rechercher un film, une série..." autocomplete="off">
-            <button type="submit" class="go-icon" title="Lancer la recherche !"><i class="fas fa-search"></i></button>
+            <input type="search" name="searchdata" id="searchdata" onkeyup="showMovie(this.value)"
+                   placeholder="Rechercher un film, une série..." autocomplete="off">
+            <button type="submit" class="go-icon" title="Lancer la recherche !"><i class="fas fa-search"></i>
+            </button>
             <div class="dropdown--searchbar" id="txtMovie"></div>
         </form>
     </nav>
