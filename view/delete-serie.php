6966127ee413df1e72cae19e1a668864549c53b9
Alan THOB <alan.thob@hotmail.fr>
2023-05-26 12:56:58 +0200

    Update Delete serie UI

--- view/delete-serie.php
+++ view/delete-serie.php
@@ -19,18 +19,55 @@ if(isset($_SESSION['user_role'])){
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang=fr>
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modifier</title>
+    <meta name="description"
+          content="Films, livres, audios | Toute une bibliothèque pour vous divertir, où que vous soyez, en illimité !"/>
+    <meta name="robots" content="index, follow"/>
+    <meta property="og:title" content="Supprimer une série | e-Gnose"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:image" content="https://e-gnose.sfait.fr/assets/img/favicon.png"/>
+    <meta property="og:description"
+          content="Films, livres, audios | Toute une bibliothèque pour vous divertir, où que vous soyez, en illimité !"/>
+    <meta property="og:locale" content="fr_FR"/>
+    <meta name="twitter:card" content="summary"/>
+    <meta name="twitter:title" content="Supprimer une série | e-Gnose"/>
+    <meta name="twitter:description"
+          content="Films, livres, audios | Toute une bibliothèque pour vous divertir, où que vous soyez, en illimité !"/>
+    <meta name="twitter:image" content="https://e-gnose.sfait.fr/assets/img/favicon.png"/>
+    <title>Supprimer une série | e-Gnose</title>
+
+    <!-- Favicons -->
+    <link href="../assets/img/favicon.ico" rel="icon">
+
+    <!-- Links -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet">
+    <link href="https://cdn.usebootstrap.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href='https://unicons.iconscout.com/release/v2.1.9/css/unicons.css' rel="stylesheet">
+    <link href="../assets/css/modify-pages.css" rel="stylesheet" type="text/css" media="screen">
+    <script src="https://kit.fontawesome.com/d51f8b0cc0.js" crossorigin="anonymous" defer></script>
 </head>
 
-<body>
-    <center>
-        <p>Veuillez selectionner le serie à supprimer :</p>
+<body class="unselectable">
+
+<?php
+include_once('../_navbar/navbar.php');
+?>
+
+<section>
+    <div class="container">
+        <div class="title">
+            <h1>Supprimer une série</h1>
+        </div>
+
+        <div class="user_infos--container">
+            <p>Sélectionnez la série à supprimer :</p>
         <form action="" method='post'>
             <?php
             // On récupere tout le contenu de la table clients
@@ -47,7 +84,7 @@ if(isset($_SESSION['user_role'])){
                     echo '<option value="' . $row['id_serie'] . '">' . $row['id_serie'] . ' - ' . $row['serie_titre'] . '</option>';
                 } ?>
             </select>
-            <input type="submit" value="Valider" /><br />
+            <input class="subscribe__btn" type="submit" value="Valider"/>
         </form></br>
 
         <?php
@@ -59,25 +96,82 @@ if(isset($_SESSION['user_role'])){
             echo "Vous voulez supprimer : " . $_POST['id'] . ' - ' . $donnees['serie_titre'] . " ?</br>";
             echo '<form action="" method="post">';
             // On affiche chaque entrée une à une
-           
-            echo '<input type="int" name="id_serie" value="' . $donnees['id_serie'] . '" readonly>' . "<br>";
-            echo '<input type="text" name="serie_city" value="' . $donnees['serie_city'] . '" readonly>' . "<br>";
-            echo '<input type="text" name="serie_titre" value="' . $donnees['serie_titre'] . '" readonly>' . "<br>";
-            echo '<input type="text" name="serie_realisateur" value="' . $donnees['serie_realisateur'] . '" readonly>' . "<br>";
-            echo '<input type="text" name="serie_description_plus" value="' . $donnees['serie_description_plus'] . '" readonly>' . "<br>";
-            echo '<input type="text" name="serie_duree_episode" value="' . $donnees['serie_duree_episode'] . '" readonly>' . "<br>";
-            echo '<input type="text" name="serie_cover_image" value="' . $donnees['serie_cover_image'] . '" readonly>' . "<br>";
-            echo '<input type="text" name="serie_back_cover" value="' . $donnees['serie_back_cover'] . '" readonly>' . "<br>";
-            echo '<input type="text" name="serie_trailer" value="' . $donnees['serie_trailer'] . '" readonly>' . "<br>";
-            echo '<input type="text" name="serie_nombre_episodes" value="' . $donnees['serie_nombre_episodes'] . '" readonly>' . "<br>";
-            echo '<input type="text" name="serie_nombre_saison" value="' . $donnees['serie_nombre_saison'] . '" readonly>' . "<br>";
-            echo '<input type="float" name="serie_popularity" value="' . $donnees['serie_popularity'] . '" readonly>' . "<br>";
-            echo '<input type="int" name="serie_value" value="' . $donnees['serie_value'] . '"  min="0" max="1" readonly>' . "<br>";
-            echo "<input type='submit' value='supprimer définitivement' /><br />";
+            echo'<div class="form-group">
+            
+            <div class="form-group">
+            <label for="id_serie">ID Série</label>
+                <input type="int" class="form-control" name="id_serie" value="' . $donnees['id_serie'] . '" readonly>
+            </div>
+
+            <div class="form-group">
+            <label for="serie_city">Ville</label>
+                <input type="text" class="form-control" name="serie_city" value="' . $donnees['serie_city'] . '" readonly>
+            </div>
+            
+            <div class="form-group">
+            <label for="serie_titre">Titre de la série</label>
+                <input type="text" class="form-control" name="serie_titre" value="' . $donnees['serie_titre'] . '" readonly>
+            </div>
+            
+            <div class="form-group">
+            <label for="serie_realisateur">Réalisateur</label>
+                <input type="text" class="form-control" name="serie_realisateur" value="' . $donnees['serie_realisateur'] . '" readonly>
+            </div>
+            
+            <div class="form-group">
+            <label for="serie_description_plus">Description</label>
+                <input type="text" class="form-control" name="serie_description_plus" value="' . $donnees['serie_description_plus'] . '" readonly>
+            </div>
+            
+            <div class="form-group">
+            <label for="serie_duree_episode">Durée d\'un épisode</label>
+                <input type="text" class="form-control" name="serie_duree_episode" value="' . $donnees['serie_duree_episode'] . '" readonly>
+            </div>
+            
+            <div class="form-group">
+            <label for="serie_cover_image">Image de couverture</label>
+                <input type="text" class="form-control" name="serie_cover_image" value="' . $donnees['serie_cover_image'] . '" readonly>
+            </div>
+            
+            <div class="form-group">
+            <label for="serie_back_cover">Image de couverture arrière</label>
+                <input type="text" class="form-control" name="serie_back_cover" value="' . $donnees['serie_back_cover'] . '" readonly>
+            </div>
+            
+            <div class="form-group">
+            <label for="serie_trailer">Bande annonce</label>
+                <input type="text" class="form-control" name="serie_trailer" value="' . $donnees['serie_trailer'] . '" readonly>
+            </div>
+            
+            <div class="form-group">
+            <label for="serie_nombre_episodes">Nombre d\'épisodes</label>
+                <input type="text" class="form-control" name="serie_nombre_episodes" value="' . $donnees['serie_nombre_episodes'] . '" readonly>
+            </div>
+            
+            <div class="form-group">
+            <label for="serie_nombre_saison">Nombre de saisons</label>
+                <input type="text" class="form-control" name="serie_nombre_saison" value="' . $donnees['serie_nombre_saison'] . '" readonly>
+            </div>
+            
+            <div class="form-group">
+            <label for="serie_popularity">Popularité</label>
+                <input type="float" class="form-control" name="serie_popularity" value="' . $donnees['serie_popularity'] . '" readonly>
+            </div>
+            
+            <div class="form-group">
+            <label for="serie_value">Valeur</label>
+                <input type="int" class="form-control" name="serie_value" value="' . $donnees['serie_value'] . '" min="0" max="1" readonly>
+            </div>';
+            
+            echo '<input style="width: 15rem;" class="subscribe__btn" type="submit" value="Supprimer définitivement">';
             echo "</form>";
+
         }
         $administration->DeleteSerie();
         ?>
+        </div>
+    </div>
+</section>
 </body>
 
 </html>
